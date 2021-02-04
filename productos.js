b75465d0f16ffbaca6397d5f372ce5912d3fd6de
ivannahuel <ivan.s@embluemail.com>
2021-02-04 14:23:33 -0300

    cambios en los estilos y ademas arreglo la vista de las tarjetas de informacion que estas solapaban a las tarjetas padres de abajo

--- productos.js
+++ productos.js
@@ -1,7 +1,18 @@
+function VerificarEstadoYSetearIndexNegativo (idProducto){
+    for (var x=1; x<7; x++ ){
+        if (idProducto != 'producto-' + x && idProducto != 'producto-' + x + '-info' ){
+            $('#producto-' + x).attr('style','z-index: 0');
+        } else {
+            $('#producto-' + x).attr('style','z-index: 1');
+        }
+    }
+}
+
 $(document).ready(function(){
-    $('#producto-1 ,#producto-1-info').mouseover(function(){
-        $('#producto-1-info').css({'opacity':'1','top':'200px','border-radius':'5px'});
-        $('#producto-1').css('z-index','15');
+    $('#producto-1 ,#producto-1-info').mouseover(function(el){
+        $('#producto-1-info').css({'opacity':'1','top':'200px','border-radius':'5px','z-index':'95'});
+        //$('#producto-1').css('z-index','15');
+        VerificarEstadoYSetearIndexNegativo(el.currentTarget.id);
         event.stopPropagation();
 
     });
@@ -11,9 +22,10 @@ $(document).ready(function(){
         event.stopPropagation()
     });
 
-    $('#producto-2 ,#producto-2-info').mouseover(function(){
+    $('#producto-2 ,#producto-2-info').mouseover(function(el){
         $('#producto-2-info').css({'opacity':'1','top':'200px','border-radius':'5px'});
-        $('#producto-2').css('z-index','15');
+        //$('#producto-2').css('z-index','15');
+        VerificarEstadoYSetearIndexNegativo(el.currentTarget.id);
         event.stopPropagation();
 
     });
@@ -23,9 +35,10 @@ $(document).ready(function(){
         event.stopPropagation()
     });
 
-    $('#producto-3 ,#producto-3-info').mouseover(function(){
+    $('#producto-3 ,#producto-3-info').mouseover(function(el){
         $('#producto-3-info').css({'opacity':'1','top':'200px','border-radius':'5px'});
-        $('#producto-3').css('z-index','15');
+        //$('#producto-3').css('z-index','15');
+        VerificarEstadoYSetearIndexNegativo(el.currentTarget.id);
         event.stopPropagation();
 
     });
@@ -35,14 +48,10 @@ $(document).ready(function(){
         event.stopPropagation()
     });
 
-    $('#producto-4 ,#producto-4-info').mouseover(function(){
+    $('#producto-4 ,#producto-4-info').mouseover(function(el){
         $('#producto-4-info').css({'opacity':'1','top':'200px','border-radius':'5px'});
-        $('#producto-4').css('z-index','1');
-        /*
-        $("html, body").animate({
-            scrollTop: 290
-        }, 2000);
-        */
+        //$('#producto-4').css('z-index','1');
+        VerificarEstadoYSetearIndexNegativo(el.currentTarget.id);
         event.stopPropagation();
 
     });
@@ -52,14 +61,10 @@ $(document).ready(function(){
         event.stopPropagation()
     });
 
-    $('#producto-5 ,#producto-5-info').mouseover(function(){
+    $('#producto-5 ,#producto-5-info').mouseover(function(el){
         $('#producto-5-info').css({'opacity':'1','top':'200px','border-radius':'5px'});
-        $('#producto-5').css('z-index','1');
-        /*
-        $("html, body").animate({
-            scrollTop: 290
-        }, 2000);
-        */
+        //$('#producto-5').css('z-index','1');
+        VerificarEstadoYSetearIndexNegativo(el.currentTarget.id);
         event.stopPropagation();
     });
 
@@ -68,14 +73,10 @@ $(document).ready(function(){
         event.stopPropagation()
     });
 
-    $('#producto-6 ,#producto-6-info').mouseover(function(){
+    $('#producto-6 ,#producto-6-info').mouseover(function(el){
         $('#producto-6-info').css({'opacity':'1','top':'200px','border-radius':'5px'});
-        $('#producto-6').css('z-index','1');
-        /*
-        $("html, body").animate({
-            scrollTop: 290
-        }, 2000);
-        */
+        //$('#producto-6').css('z-index','1');
+        VerificarEstadoYSetearIndexNegativo(el.currentTarget.id);
         event.stopPropagation();
     });
 
@@ -84,6 +85,7 @@ $(document).ready(function(){
         event.stopPropagation()
     });
 
-
+    //setear por defecto que al producto que hicimos click no deberia 
+    //aplicarle el z-index de -1
 
  });
